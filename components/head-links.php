eb65b7c45f852f513ef3ee196c304094580d0dd0
mss-souloftware <sufyan.souloftware@gmail.com>
2024-03-16 13:36:26 +0500

    booking form

--- components/head-links.php
+++ components/head-links.php
@@ -1,3 +1,3 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
-<link rel="stylesheet" href="assets/css/bootstrap.css">
-<link rel="stylesheet" href="assets/css/style.css"
+<link rel="stylesheet" href="assets/css/style.css">
