0d05a0847cfa224d14b33c353fd1a44d3ff8e40f
mss-souloftware <sufyan.souloftware@gmail.com>
2024-03-19 10:08:17 +0500

    second step

---
+++ components/booking/step2.php
@@ -0,0 +1,37 @@
+<div class="row">
+    <div class="col-md-5">
+        <h3>Select Vehicle</h3>
+        <div class="selectCar">
+            <div class="carCard">
+                <div class="carIMg">
+                    <img src="assets/img/Saloon-thumb.webp" alt="Saloon">
+                </div>
+                <div class="carData">
+                    <div class="titleRow">
+                        <h4>Saloon</h4>
+                        <span>Select</span>
+                    </div>
+                    <h2>£234</h2>
+                    <div class="descData">
+                        <p>
+                            Up to 3 passengers plus 3 standard suitcases (23kg max),
+                            or 4 passengers plus hand luggage. Ford Mondeo or VW
+                            Passat or similar.
+                        </p>
+                        <ul>
+                            <li>
+                                <img src="assets/img/luggage.png" alt="Luggage">
+                                <span>4</span>
+                            </li>
+                            <li>
+                                <img src="assets/img/passenger.png" alt="Passengers">
+                                <span>4</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-7"></div>
+</div>   
